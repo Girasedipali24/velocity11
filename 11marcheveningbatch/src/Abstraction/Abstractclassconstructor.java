3ec5e0c05ad0ea1df1c08865c7a19604c8980239
girase dipali <girasedipali3@gmail.com>
2023-08-03 12:54:59 +0530

    first commit

---
+++ 11marcheveningbatch/src/Abstraction/Abstractclassconstructor.java
@@ -0,0 +1,37 @@
+package Abstraction;
+
+public abstract class Abstractclassconstructor {
+    public Abstractclassconstructor() {
+    	this(300);
+    	
+    	System.out.println("Abstract class constructor");
+    }
+
+    
+        private Abstractclassconstructor(int i) {
+        	
+        	System.out.println("one input parameter constructor");
+        }
+        
+        private Abstractclassconstructor(int i,int j) {
+        	
+        	System.out.println("Two input parameter constructor");
+        	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 11marcheveningbatch/src/constructor/constructor1.java
@@ -0,0 +1,15 @@
+package constructor;
+
+public class constructor1 {
+	public static void main(String [] args ) {
+		constructor1 v2 =new constructor1();
+		
+	}
+	public constructor1() {
+		System.out.println("hello world");
+		System.out.println("my name is dipali ");
+		
+	}
+	
+
+}
